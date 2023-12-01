f47aeb6cb767399c00f9a8f9bc970c6df9c88983
Om <ommukherjee28@gmail.com>
2023-12-01 15:53:13 +0530

    initial commit

--- components/About.tsx
+++ components/About.tsx
@@ -31,7 +31,7 @@ export default function About({}: Props) {
             </h4>
             <p className="text-sm max-w-xl">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            Lorem Ipsum has been the industry standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book.\
             It has survived not only five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
