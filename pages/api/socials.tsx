93103bf9d16bce1e0215a2a1364b467d8580d267
Om <ommukherjee28@gmail.com>
2023-12-02 18:55:43 +0530

    Changes in api

--- pages/api/socials.tsx
+++ pages/api/socials.tsx
@@ -1,25 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {groq} from 'next-sanity'
-import {sanityClient} from '../../sanity';
-import {Social} from '../../typings';
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import {groq} from 'next-sanity'
+// import {sanityClient} from '../../sanity';
+// import {Social} from '../../typings';
 
-const query = groq`
-    *[_type == "social"]
-`
-type Data = {
-    socials:Social[]
-}
+// const query = groq`
+//     *[_type == "social"]
+// `
+// type Data = {
+//     socials:Social[]
+// }
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse<Data>)
-{
-    if(req.method !='GET'){
-        return res.status(405).end();
-    }
-    try{
-        const socials:Social[] = await sanityClient.fetch(query);
-        return res.status(200).json({socials});
-    }catch(error){
-        console.log(error);
-        return res.status(500).end();
-    }
-}
+// export default async function handler(req:NextApiRequest, res:NextApiResponse<Data>)
+// {
+//     if(req.method !='GET'){
+//         return res.status(405).end();
+//     }
+//     try{
+//         const socials:Social[] = await sanityClient.fetch(query);
+//         return res.status(200).json({socials});
+//     }catch(error){
+//         console.log(error);
+//         return res.status(500).end();
+//     }
+// }
