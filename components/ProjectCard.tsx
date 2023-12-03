dc65271ec727f00c500b15d285f77ba2a9f7deef
Om <ommukherjee28@gmail.com>
2023-12-04 01:13:56 +0530

    Project Image With fetchPriority high

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -12,12 +12,14 @@ function ProjectCard({project,projects}: Props) {
   return (
     <div className="flex flex-col w-full  items-center justify-center  h-full ">
         <motion.img
+        fetchPriority="high"
         initial={{y:-300,opacity:0}}
         transition={{duration:1.2}}
         whileInView={{y:0,opacity:1}}
         viewport={{once:true}}
         className="mt-20 md:mt-0 w-[600px] h-auto" 
         src={urlFor(project?.image).url()}
+
         alt="project asset" />
         <div className="px-0 md:px-10 max-w-6xl">
             <h4 className="text-xl py-4 sm:text-3xl font-semibold text-center uppercase">
